be6640cd944614c1c3856d61344c790c411f8ef7
Chrono <chrono_legionnaire@protonmail.com>
2026-01-01 16:21:50 +0800

    Create sodiumhashsha512.go

---
+++ Source/Version 0.0.1/sodiumhashsha512/sodiumhashsha512.go
@@ -0,0 +1,103 @@
+package sodiumhashsha512
+
+// #cgo pkg-config: libsodium
+// #include <sodium.h>
+import "C"
+import (
+	"fmt"
+	"unsafe"
+)
+
+func GetComputedHashLength() int {
+	return int(C.crypto_hash_sha512_bytes())
+}
+
+func GetStateBytesLength() int {
+	return int(C.crypto_hash_sha512_statebytes())
+}
+
+func ComputeHash(message []byte) ([]byte, error) {
+	if message == nil {
+		return nil, fmt.Errorf("message must not be nil")
+	}
+
+	out := make([]byte, GetComputedHashLength())
+
+	rc := C.crypto_hash_sha512(
+		(*C.uchar)(unsafe.Pointer(&out[0])),
+		(*C.uchar)(unsafe.Pointer(&message[0])),
+		C.ulonglong(len(message)),
+	)
+
+	if rc != 0 {
+		return nil, fmt.Errorf("failed to compute hash using SHA-256")
+	}
+
+	return out, nil
+}
+
+func InitializeState() ([]byte, error) {
+	state := make([]byte, GetStateBytesLength())
+
+	rc := C.crypto_hash_sha512_init(
+		(*C.crypto_hash_sha512_state)(unsafe.Pointer(&state[0])),
+	)
+
+	if rc != 0 {
+		return nil, fmt.Errorf("failed to initialize SHA-256 state")
+	}
+
+	return state, nil
+}
+
+func UpdateState(state []byte, message []byte) ([]byte, error) {
+	if state == nil {
+		return nil, fmt.Errorf("state must not be nil")
+	}
+	if len(state) != GetStateBytesLength() {
+		return nil, fmt.Errorf(
+			"state length must be %d bytes",
+			GetStateBytesLength(),
+		)
+	}
+	if message == nil {
+		return nil, fmt.Errorf("message must not be nil")
+	}
+
+	rc := C.crypto_hash_sha512_update(
+		(*C.crypto_hash_sha512_state)(unsafe.Pointer(&state[0])),
+		(*C.uchar)(unsafe.Pointer(&message[0])),
+		C.ulonglong(len(message)),
+	)
+
+	if rc != 0 {
+		return nil, fmt.Errorf("failed to update SHA-256 state")
+	}
+
+	return state, nil
+}
+
+func ComputeHashForFinalizedState(state []byte) ([]byte, error) {
+	if state == nil {
+		return nil, fmt.Errorf("state must not be nil")
+	}
+	if len(state) != GetStateBytesLength() {
+		return nil, fmt.Errorf(
+			"state length must be %d bytes",
+			GetStateBytesLength(),
+		)
+	}
+
+	out := make([]byte, GetComputedHashLength())
+
+	rc := C.crypto_hash_sha512_final(
+		(*C.crypto_hash_sha512_state)(unsafe.Pointer(&state[0])),
+		(*C.uchar)(unsafe.Pointer(&out[0])),
+	)
+
+	if rc != 0 {
+		return nil, fmt.Errorf("failed to finalize SHA-256 hash")
+	}
+
+	return out, nil
+}
